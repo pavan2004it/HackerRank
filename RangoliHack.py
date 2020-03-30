940e8979d5f1178bef05e1e0a343d1734a8e8f07
Pavan.Tikkani <pavan.tikkani@bioclinica.com>
2020-03-30 12:03:52 +0530

    Solved the challenge

--- RangoliHack.py
+++ RangoliHack.py
@@ -1,7 +1,84 @@
-# import string
-N = int(input())
-M = ((N*N)-1) / 2
-print(M)
+"""
+#size 3
 
-# print("-" * int(M) + string.ascii_lowercase[2:3] +
-# " " + string.ascii_lowercase[1:2] + " " + string.ascii_lowercase[2:3])
+----c----
+--c-b-c--
+c-b-a-b-c
+--c-b-c--
+----c----
+
+#size 5
+
+--------e--------
+------e-d-e------
+----e-d-c-d-e----
+--e-d-c-b-c-d-e--
+e-d-c-b-a-b-c-d-e
+--e-d-c-b-c-d-e--
+----e-d-c-d-e----
+------e-d-e------
+--------e--------
+
+#size 10
+
+------------------j------------------
+----------------j-i-j----------------
+--------------j-i-h-i-j--------------
+------------j-i-h-g-h-i-j------------
+----------j-i-h-g-f-g-h-i-j----------
+--------j-i-h-g-f-e-f-g-h-i-j--------
+------j-i-h-g-f-e-d-e-f-g-h-i-j------
+----j-i-h-g-f-e-d-c-d-e-f-g-h-i-j----
+--j-i-h-g-f-e-d-c-b-c-d-e-f-g-h-i-j--
+j-i-h-g-f-e-d-c-b-a-b-c-d-e-f-g-h-i-j
+--j-i-h-g-f-e-d-c-b-c-d-e-f-g-h-i-j--
+----j-i-h-g-f-e-d-c-d-e-f-g-h-i-j----
+------j-i-h-g-f-e-d-e-f-g-h-i-j------
+--------j-i-h-g-f-e-f-g-h-i-j--------
+----------j-i-h-g-f-g-h-i-j----------
+------------j-i-h-g-h-i-j------------
+--------------j-i-h-i-j--------------
+----------------j-i-j----------------
+------------------j------------------
+
+
+"""
+
+number = int(input())
+N = (number * 3) + (number - 3)
+M = 1
+x = number - 1
+vertical_lines = number + (number - 1)
+alpha = [chr(x) for x in range(ord('a'), ord('z') + 1)]
+
+
+for i in range(vertical_lines):
+    if number == 1:
+        print(alpha[number-1])
+
+    elif x > 0 and i < (number - 1):
+        if len(('-'.join(alpha[x:number]))) > 1:
+            print('-' * int(vertical_lines - M) + ('-'.join(alpha[x + 1:number])[::-1]) + '-' +
+                  ('-'.join(alpha[x:number])) + '-' * int(vertical_lines - M))
+            M += 2
+            x -= 1
+        else:
+            print('-' * int(vertical_lines - M) +
+                  ('-'.join(alpha[x:number])) + '-' * int(vertical_lines - M))
+            M += 2
+            x -= 1
+    elif i == number - 1:
+        print('-' * int(vertical_lines - M) + ('-'.join(alpha[x + 1:number])[::-1]) + '-' +
+              ('-'.join(alpha[x:number])) + '-' * int(vertical_lines - M))
+
+    elif i > (number - 1):
+        M -= 2
+        x += 1
+        if len(('-'.join(alpha[x:number]))) > 1:
+            print('-' * int(vertical_lines - M) + ('-'.join(alpha[x + 1:number])[::-1]) + '-' +
+                  ('-'.join(alpha[x:number])) + '-' * int(vertical_lines - M))
+        else:
+            print('-' * int(vertical_lines - M) +
+                  ('-'.join(alpha[x:number])) + '-' * int(vertical_lines - M))
+
+
