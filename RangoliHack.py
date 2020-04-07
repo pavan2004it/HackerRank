e157202579b88b8ad24c80500d0d609d371ddc99
Pavan.Tikkani <pavan.tikkani@bioclinica.com>
2020-04-07 10:54:27 +0530

    Solved the challenge

--- RangoliHack.py
+++ RangoliHack.py
@@ -44,39 +44,60 @@ j-i-h-g-f-e-d-c-b-a-b-c-d-e-f-g-h-i-j
 
 """
 
-number = int(input())
-M: int = 1
-x = number - 1
-vertical_lines = number + (number - 1)
-alpha = [chr(x) for x in range(ord('a'), ord('z') + 1)]
-
-for i in range(vertical_lines):
-    if number == 1:
-        print(alpha[number-1])
-
-    elif x > 0 and i < (number - 1):
-        if len(('-'.join(alpha[x:number]))) > 1:
-            print('-' * int(vertical_lines - M) + ('-'.join(alpha[x + 1:number])[::-1]) + '-' +
-                  ('-'.join(alpha[x:number])) + '-' * int(vertical_lines - M))
-            M += 2
-            x -= 1
-        else:
-            print('-' * int(vertical_lines - M) +
-                  ('-'.join(alpha[x:number])) + '-' * int(vertical_lines - M))
-            M += 2
-            x -= 1
-    elif i == number - 1:
-        print('-' * int(vertical_lines - M) + ('-'.join(alpha[x + 1:number])[::-1]) + '-' +
-              ('-'.join(alpha[x:number])) + '-' * int(vertical_lines - M))
-
-    elif i > (number - 1):
-        M -= 2
-        x += 1
-        if len(('-'.join(alpha[x:number]))) > 1:
-            print('-' * int(vertical_lines - M) + ('-'.join(alpha[x + 1:number])[::-1]) + '-' +
-                  ('-'.join(alpha[x:number])) + '-' * int(vertical_lines - M))
-        else:
-            print('-' * int(vertical_lines - M) +
-                  ('-'.join(alpha[x:number])) + '-' * int(vertical_lines - M))
+import timeit
+import string
 
 
+def sol_one():
+    number = 5
+    m: int = 1
+    x = number - 1
+    vertical_lines = number + (number - 1)
+    alpha = [chr(x) for x in range(ord('a'), ord('z') + 1)]
+
+    for i in range(vertical_lines):
+        if number == 1:
+            print(alpha[number-1])
+
+        elif x > 0 and i < (number - 1):
+            if len(('-'.join(alpha[x:number]))) > 1:
+                print('-' * int(vertical_lines - m) + ('-'.join(alpha[x + 1:number])[::-1]) + '-' +
+                      ('-'.join(alpha[x:number])) + '-' * int(vertical_lines - m))
+                m += 2
+                x -= 1
+            else:
+                print('-' * int(vertical_lines - m) +
+                      ('-'.join(alpha[x:number])) + '-' * int(vertical_lines - m))
+                m += 2
+                x -= 1
+        elif i == number - 1:
+            print('-' * int(vertical_lines - m) + ('-'.join(alpha[x + 1:number])[::-1]) + '-' +
+                  ('-'.join(alpha[x:number])) + '-' * int(vertical_lines - m))
+
+        elif i > (number - 1):
+            m -= 2
+            x += 1
+            if len(('-'.join(alpha[x:number]))) > 1:
+                print('-' * int(vertical_lines - m) + ('-'.join(alpha[x + 1:number])[::-1]) + '-' +
+                      ('-'.join(alpha[x:number])) + '-' * int(vertical_lines - m))
+            else:
+                print('-' * int(vertical_lines - m) +
+                      ('-'.join(alpha[x:number])) + '-' * int(vertical_lines - m))
+
+
+def sol_two():
+    alpha = string.ascii_lowercase
+
+    n = 5
+    L = []
+    for i in range(n):
+        s = "-".join(alpha[i:n])
+        L.append((s[::-1] + s[1:]).center(4 * n - 3, "-"))
+    print('\n'.join(L[:0:-1] + L))
+
+
+res_1 = timeit.timeit(sol_one, number=10000)
+res_2 = timeit.timeit(sol_two, number=10000)
+
+print(res_1)
+print(res_2)
---
+++ setchallenge-3.py
@@ -0,0 +1,3 @@
+number_of_countries = int(input())
+set_country = {input() for x in range(number_of_countries)}
+print(len(set_country))
