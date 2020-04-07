b517c5958d4f1ab3a36a6414348244e8c5e7c384
Pavan.Tikkani <pavan.tikkani@bioclinica.com>
2020-04-07 15:05:16 +0530

    Solved the challenge

---
+++ captainshack.py
@@ -0,0 +1,7 @@
+import collections
+k = '1 2 3 6 5 4 4 2 5 3 6 1 6 5 3 2 4 1 2 5 1 4 3 6 8 4 3 1 5 6 2'
+list_a = [x for x in k.split()]
+counter = collections.Counter(list_a)
+for k,v in counter.items():
+    if v == 1:
+        print(k)
